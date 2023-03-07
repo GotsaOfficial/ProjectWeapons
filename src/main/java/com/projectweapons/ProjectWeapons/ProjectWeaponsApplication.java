085f9d572fbb1a1f4d782979ca8a58577667bbdf
GotsaOfficial <gotsa.vlad@gmail.com>
2023-03-07 23:02:15 +0200

    Initial commit

---
+++ src/main/java/com/projectweapons/ProjectWeapons/ProjectWeaponsApplication.java
@@ -0,0 +1,13 @@
+package com.projectweapons.ProjectWeapons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWeaponsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWeaponsApplication.class, args);
+	}
+
+}
